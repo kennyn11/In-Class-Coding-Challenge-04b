daf3dc4ff3ac9acfe48dfdeac6e410b734a394be
Kenny Nguyen <kennyn@usf.edu>
2026-02-09 13:16:39 -0500

    initial commit

---
+++ cc_4b.js
@@ -0,0 +1,7 @@
+const products = [
+  { sku: "sku-001", name: "Tea", category: "groceries", price: 10.00, inventory: 60 },
+  { sku: "sku-002", name: "Coffee", category: "groceries", price: 15.00, inventory: 100 },
+  { sku: "sku-003", name: "Organic Coffee", category: "groceries", price: 18.00, inventory: 50 },
+  { sku: "sku-004", name: "Laundry Detergent", category: "household", price: 20.00, inventory: 30 },
+  { sku: "sku-005", name: "Notebook", category: "stationary", price: 5.00, inventory: 150 }
+];
